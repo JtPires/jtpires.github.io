d531716960849413dbc2625e1f38875691c06443
JtPires <jpires1@uncc.edu>
2021-09-24 12:48:17 -0400

    Update alert.js

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -2,3 +2,10 @@ function myAlert(){
 alert("Hey my script is running");
 }
 
+const date = new Date();
+let comp = "Pires Inc.";
+var user = prompt("Please enter your name");
+var feeling = prompt("How are you you doing");
+document.write("Today is " + date);
+document.write("The " + comp + "welcomes you, " + user + "!");
+document.write("We're glad you are doing " + feeling + "!");
