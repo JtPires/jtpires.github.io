62a7164b17a59f4b124c2f45028427238024e3b2
JtPires <jpires1@uncc.edu>
2021-10-03 19:35:11 -0400

    Create calculator_try.js

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,39 @@
+function myAlert(){
+alert("7");
+}
+function alert8(){
+alert("8");
+}
+function alert9(){
+alert("9");
+}
+function alert4(){
+alert("4");
+}
+function alert5(){
+alert("5");
+}
+function alert6(){
+alert("6");
+}
+function alert1(){
+alert("1");
+}
+function alert2(){
+alert("2");
+}
+function alert3(){
+alert("3");
+}
+function alertPlus(){
+alert("+");
+}
+function alertMinus(){
+alert("-");
+}
+function alertClear(){
+alert("AC");
+}
+function alertDiv(){
+alert("%");
+}
