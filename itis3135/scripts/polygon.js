34df7ef0b74a839afb0d4841a2af658ad5fdd154
JtPires <jpires1@uncc.edu>
2021-10-23 20:40:54 -0400

    Update polygon.js

--- itis3135/scripts/polygon.js
+++ itis3135/scripts/polygon.js
@@ -1,8 +1,8 @@
 
 //Ask the user to input a number
-number_sides = window.prompt("The Joyful Penguin would like your number");
-validateEntry(number_sides);
-getShape(number_sides);
+number_sides = window.prompt("The Joyful Penguin would like your number")
+valid_sides = validateEntry(number_sides);
+getShape(valid_sides);
 
 
 
@@ -12,64 +12,44 @@ function getShape(number_sides){
 //Print names of the polygon that matches the side number
 
 if(number_sides == 1){
-document.write("This is a Henagon! ");
+alert("This is a Henagon! ");
 }else if (number_sides == 2){
-document.write("This is a Digon!");
+alert("This is a Digon!");
 }else if (number_sides == 3){
-document.write("This is a Trigon!");
+alert("This is a Trigon!");
 }else if (number_sides == 4){
-document.write("This is a Tetragon");
+alert("This is a Tetragon");
 }else if (number_sides == 5){
-document.write("This is a Pentagon!");
+alert("This is a Pentagon!");
 }else if (number_sides == 6){
-document.write("This is a Hexagon!");
+alert("This is a Hexagon!");
 }else if (number_sides == 7){
-document.write("This is a Heptagon!");
+alert("This is a Heptagon!");
 }else if (number_sides == 8){
-document.write("This is a Octagon!");
+alert("This is a Octagon!");
 }else if (number_sides == 9){
-document.write("This is a Nonagon!");
+alert("This is a Nonagon!");
 }else if (number_sides == 10){
-document.write("This is a Decagon!");
+alert("This is a Decagon!");
 }
 
 }
 
 
 
-function validateEntry(number){
+function validateEntry(number_sides){
 
+valid_sides = Math.abs(number_sides);
+valid_sides = Math.round(valid_sides);
 
-//If number is a string alert the user this is invalid
-
-if(isNaN(number)){
-alert("Invalid statement");
+do{
+if(valid_sides < 0 || valid_sides > 10){
+valid_sides = alert("Invalid Input");
 }
-
-if(number <= -10.4){
-alert("Invalid number");
+} 
+while (valid_sides > 10)
+return valid_sides;
 }
-
-if(number < 1){
-return number_sides = Math.abs(number_sides);
-
-}
-
-if(number > 10.5){
-alert("Invalid number");
-}
-
-//If decimal round to nearest integer
-
-if(number > 0.5 || number < 10.5){
-return number_sides = Math.round(number_sides);
-
-}
-
-
- 
-}
-
 
 
 
